2f9d9d4d5eb61b2aabe7df3493b20270fc6e6c15
Sumit <sumitnarayan9091@gmail.com>
2024-08-06 15:07:08 +0530

    firebase fixed

--- app/page.js
+++ app/page.js
@@ -1,95 +1,207 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  Button,
+  Modal,
+  TextField,
+  boxClasses,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import { firestore } from "@/firebase";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from "firebase/firestore";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "white",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: 3,
+};
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [itemName, setItemName] = useState("");
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data() });
+    });
+    setInventory(inventoryList);
+  };
+
+  useEffect(() => {
+    if (inventory.length === 0) updateInventory();
+  }, []);
+
+  useEffect(() => {
+    if (!itemName) return;
+    const filtered = inventory.filter(
+      (item) => item.name.toLowerCase().indexOf(itemName.toLowerCase()) > -1
+    );
+    setFilteredSuggestions(filtered);
+  }, [itemName]);
+
+  const addItem = async (item) => {
+    if (!item) return;
+    // const index = inventory.find((itm) => itm === item);
+    // if (index) {
+    //   inventory[index]++;
+    // }
+
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1 });
+    } else {
+      await setDoc(docRef, { quantity: 1 });
+    }
+    await updateInventory();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+    await updateInventory();
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Box
+      width="100vw"
+      height="100vh"
+      display={"flex"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      gap={2}
+    >
+      <Box display={"flex"} textAlign={"center"} width="800px">
+        <TextField
+          id="outlined-basic"
+          label="Item"
+          variant="outlined"
+          fullWidth
+          value={itemName}
+          onChange={(e) => {
+            setItemName(e.target.value);
+          }}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          color="secondary"
+          variant="contained"
+          onClick={() => {
+            addItem(itemName);
+            setItemName("");
+          }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          Add
+        </Button>
+      </Box>
+      <Box border={"1x solid black"}>
+        {itemName && (
+          <List>
+            {filteredSuggestions.map((suggestion, index) => (
+              <ListItem
+                key={index}
+                padding={"2px"}
+                sx={{ cursor: "hand" }}
+                onClick={() => setItemName(suggestion.name)}
+              >
+                <ListItemText primary={suggestion.name} />
+              </ListItem>
+            ))}
+          </List>
+        )}
+      </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box border={"1px solid #333"}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor={"#a294fa"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          <Typography variant={"h3"} color={"#333"} textAlign={"center"}>
+            Inventory Items
+          </Typography>
+        </Box>
+        <Stack
+          width="800px"
+          min-height="0px"
+          height="300px"
+          spacing={2}
+          overflow={"auto"}
+          bgcolor={"#e3dddc"}
         >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          {inventory.map(({ name, quantity }) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="50px"
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              bgcolor={"#f0f0f0"}
+              paddingX={5}
+            >
+              <Typography variant={"h4"} color={"#333"} textAlign={"center"}>
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Box display={"flex"}>
+                <Button variant="outlined" onClick={() => addItem(name)}>
+                  ➕
+                </Button>
+                <Typography
+                  variant={"h3"}
+                  color={"#333"}
+                  textAlign={"center"}
+                  mx={"10px"}
+                >
+                  {quantity}
+                </Typography>
+                <Button variant="outlined" onClick={() => removeItem(name)}>
+                  ➖
+                </Button>
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
   );
 }
